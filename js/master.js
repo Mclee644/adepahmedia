eb97d5beb49b9ae8fc59db54fd3ad0bf6c918ba7
Mclee644 <644mcl@gmail.com>
2021-09-24 19:00:46 +0000

    auto write text

--- js/master.js
+++ js/master.js
@@ -2,19 +2,19 @@ const body=document.body;
 const app=document.getElementById('app');
 const toggle=document.getElementById('toggle');
 const header = document.getElementById('header')
+///declaration of auto write text
+const title = "Adepah Media is back online "
+const headTitle = document.getElementById('headTitle')
+const headTitles = document.querySelector('h3')
+var index = 1;
 /// declaring sidemenu
 const sideList = document.getElementById('sideMenu');
 
-if (app.onscroll) {
-    nav.style.backgroundColor='#fff';
-};
-if (app.onscroll) {
-    nav.style.backgroundColor='#fff';
-};
 /// like dis
 
 
 function sideMenu(){
+    //if(window.mouseover)
     console.log("talking to the sidebar")
     sideList.classList.toggle("active")
     toggle.classList.toggle('active')
@@ -23,3 +23,15 @@ function sideMenu(){
     ///app.classList.toggle('active')
 
 }
+
+/// auto write text script
+function awt(){
+    headTitle.innerText = title.slice(0,index) +"|"
+    index++;
+    if(index>=title.length){
+        index=0;
+
+    }
+    
+}
+setInterval(awt, 500);
